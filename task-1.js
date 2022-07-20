3663c4131c53a30b8cc1a45c81b4211d78fa689f
Maryshchyn <maryshchyn89@gmail.com>
2022-07-20 12:08:17 +0300

    2

---
+++ task-1.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+const numberOfCategories = document.querySelector('ul').children.length;
+
+console.log(`Number of categories: ${numberOfCategories}`)
+
+
+
+
+const nameEl = document.querySelector('.item').firstElementChild;
+console.log(nameEl)
+console.log(`Category: ${nameEl}`)
+const CategoryEl = document.querySelector('.item').lastElementChild;
+const numbersLength = CategoryEl.children.length;
+
+console.log(`Elements: ${numbersLength}`)
+
+
+
+
+
