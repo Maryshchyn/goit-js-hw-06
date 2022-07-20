fb159b627882ea56d45d8b112c817cfc3e41baad
Maryshchyn <maryshchyn89@gmail.com>
2022-07-20 19:46:28 +0300

    5

--- task-1.js
+++ task-1.js
@@ -4,41 +4,45 @@
 
 
 
-// const numberOfCategories = document.getElementById('categories').children.length;
-// console.log(`Number of categories: ${numberOfCategories}`)
+const numberOfCategories = document.getElementById('categories').children.length;
+console.log(`Number of categories: ${numberOfCategories}`)
 
 
 
-// const numberOfCategoriesw = document.getElementById('categories')
+const numberOfCategoriesw = document.getElementById('categories')
 
-// const firstChildren = numberOfCategoriesw.firstElementChild;
-// const firstChildrenbeginning = firstChildren.lastElementChild;
-// const firstChildrenLast = firstChildrenbeginning.children.length;
-// console.log(`Elements:${firstChildrenLast}`)
+const firstChildren = numberOfCategoriesw.firstElementChild;
+const firstChildrenbeginning = firstChildren.lastElementChild;
+const firstChildrenLast = firstChildrenbeginning.children.length;
+const firstNamebeginning = firstChildren.firstElementChild;
+const firstNameEnd = firstNamebeginning.textContent;
 
-
-// const secondChildren = numberOfCategoriesw.children[1];
-// const secondChildrenMiddle = secondChildren.children[1];
-// const secondChildrenLast = secondChildrenMiddle.children.length;
-// console.log(`Elements:${secondChildrenLast}`)
+console.log(`Category:${firstNameEnd}`)
+console.log(`Elements:${firstChildrenLast}`)
 
 
+const secondChildren = numberOfCategoriesw.children[1];
+const secondChildrenMiddle = secondChildren.children[1];
+const secondChildrenLast = secondChildrenMiddle.children.length;
+const secondChildrenName = secondChildren.children[0];
+const secondChildrenNameEnd = secondChildrenName.textContent;
 
-// const thirdchildren = numberOfCategoriesw.lastElementChild;
-// const thirdchildrenEnd = thirdchildren.lastElementChild;
-// const thirdchildrenLast = thirdchildrenEnd.children.length;
-// console.log(`Elements:${thirdchildrenLast}`)
+console.log(`Category:${secondChildrenNameEnd}`)
+console.log(`Elements:${secondChildrenLast}`)
 
 
-// const qwe = document.querySelector('.item')
-// const qwee = qwe.firstElementChild;
-// const rrr = qwee.textContent
-// console.log(`Category:${rrr}`)
 
-// const qwew = document.querySelector('.item')
-// const qweeq = qwew.lastElementChild;
-// // const rrrq = qweeq.textContent
-// console.log(qweeq)
+const thirdChildren = numberOfCategoriesw.lastElementChild;
+const thirdChildrenEnd = thirdChildren.lastElementChild;
+const thirdChildrenLast = thirdChildrenEnd.children.length;
+const thirdNameEnd = thirdChildren.firstElementChild;
+const thirdChildrenNameEnd = thirdNameEnd.textContent;
+
+console.log(`Category:${thirdChildrenNameEnd}`)
+console.log(`Elements:${thirdChildrenLast}`)
+
+
+
 
 
 
