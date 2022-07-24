70f67098a1100091d0b55ad3682e51774d941e69
Maryshchyn <maryshchyn89@gmail.com>
2022-07-24 21:01:22 +0300

    fffffffff

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,35 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",},
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",},
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",},
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
 ];
-
-
 
 const listEl = document.querySelector(`.gallery`);
 console.log(listEl);
-const createElement = image => {
+const createElement = img => {
+  return `<li> <img src="${img.url}" alt="${img.alt}" width = 200 height = 150> </li>`
   
-  return `<li> <img src="${image.url}" alt="${image.alt}"/></li>`
 }
+const galleryList = document.querySelector("#gallery");
+
+document.body.style.backgroundImage = "linear-gradient(to right, #00008B, #FFFF00)"
+
+
+
+listEl.setAttribute("style", "list-style-type:none; display: flex; justify-content: center; " );
 
 const markUp = images.map(createElement).join("")
 console.log(markUp);
 listEl.insertAdjacentHTML("beforeend", markUp)
+
+
 
